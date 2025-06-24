f5e82905d69c9b4762f8cfe1bd119f175fb17743
21F1002963 <21f1002963@ds.study.iitm.ac.in>
2025-06-24 23:27:25 +0530

    Another fix

--- FRONTEND/next.config.mjs
+++ FRONTEND/next.config.mjs
@@ -1,5 +1,5 @@
 import netlifyNext from "@netlify/next";
-const { withNetlify } = netlifyNext;
+const withNetlify = netlifyNext.default || netlifyNext;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
