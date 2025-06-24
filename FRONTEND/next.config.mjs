a17a275d3ff883501f683aad5c6a1c0f3465f97a
21F1002963 <21f1002963@ds.study.iitm.ac.in>
2025-06-25 01:06:08 +0530

    fixed duplicate

--- FRONTEND/next.config.mjs
+++ FRONTEND/next.config.mjs
@@ -12,7 +12,6 @@ const nextConfig = {
     unoptimized: true,
   },
   output: 'export',
-  trailingSlash: true,
   distDir: 'out',
 };
 
--- FRONTEND/src/lib/utils.js
+++ FRONTEND/src/lib/utils.js
@@ -7,5 +7,5 @@ export function cn(...inputs) {
 
 export function getWatchUrl(vidId, mediaType) {
   const prefix = mediaType === "tv" ? "tv" : "movie";
-  return `${prefix}/watch?id=${vidId}`;
+  return `/${prefix}/watch?id=${vidId}`;
 }
